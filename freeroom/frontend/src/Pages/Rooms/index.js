45de5acc1ecfabea73f1614d3b05174f66863da3
lucasfirmo62 <58527718+lucasfirmo62@users.noreply.github.com>
2022-04-04 10:10:25 -0300

    Add files via upload

---
+++ freeroom/frontend/src/Pages/Rooms/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const Rooms = () => {
+    return (
+        <>
+            <div id="rooms-wrapper">
+                Salas
+            </div>
+        </>
+    )
+}
+
+export default Rooms;
--- freeroom/frontend/src/routes.js
+++ freeroom/frontend/src/routes.js
@@ -3,6 +3,7 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import Login from './Pages/Login';
+import Rooms from './Pages/Rooms';
 
 export default function Routes() {
     return (
@@ -10,6 +11,7 @@ export default function Routes() {
             <Switch>
                 <Route path="/" exact component={Home} />
                 <Route path="/login" component={Login} />
+                <Route path="/rooms" component={Rooms} />
             </Switch>
         </BrowserRouter>
     )
