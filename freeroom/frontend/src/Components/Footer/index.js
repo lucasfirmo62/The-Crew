7d5d16eda0e3c7a80cdeedc65b95111cae78016b
lucasfirmo62 <58527718+lucasfirmo62@users.noreply.github.com>
2022-04-05 16:00:19 -0300

    Add files via upload

---
+++ freeroom/frontend/src/Components/Footer/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './styles.css';
+
+const Footer = () => {
+  
+    return (
+      <>
+        <div className='footer-content'>
+            <h2>Universidade Tenológica Federal do Paraná</h2>
+            <h3>Suporte ao usuário</h3>
+            <p>Utilizar o número do RA precedido da letra "a".</p>
+            <p>A senha é a mesma utilizada no Portal do aluno.</p>
+        </div>
+      </>
+    )
+  }
+  
+  export default Footer;
--- freeroom/frontend/src/Pages/Login/index.js
+++ freeroom/frontend/src/Pages/Login/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './styles.css';
+import Footer from '../../Components/Footer';
+import UTFPRLOGO from '../../Assets/utfpr-logo.png'
 
 const Login = () => {
     return (
         <>
             <div id="login-wrapper">
-                <p>Login</p>
-                <div className='login-box'>
-                    <input type="text" placeholder='seu username'></input>
-                    <input type="password" placeholder='sua senha'></input>
-                    <button>ENTRAR</button>
-                </div>
+                <img className='logo-image' src={UTFPRLOGO}/>
+                <p className='title-app'>Mapeamento de Salas do Campus Campo Mourao</p>
+                <form className='login-box'>
+                    <input type="text" placeholder='RA'></input>
+                    <input type="password" placeholder='senha'></input>
+                    <div className='click'>
+                        <button>ENTRAR</button>
+                    </div>
+                </form>
             </div>
+            <Footer/>
         </>
     )
 }
