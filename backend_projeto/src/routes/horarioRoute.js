2e911ee5bc0978ab7a6a17fee1da8e99c6d53f64
KNakao <54083799+MKNakao@users.noreply.github.com>
2022-04-05 00:19:22 -0300

    correcoes

--- backend_projeto/src/routes/horarioRoute.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import horarioController from '../controllers/horarioController.js';
-
-const router = Router();
-
-router.post('/status', horarioController.criarHorario);
-
-export default router;
